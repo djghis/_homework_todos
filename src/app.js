06c5c77f1525d8cc82cb266ac77a48a447ff661a
djghis <djghis@hotmail.com>
2020-02-24 16:13:17 +0000

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: "#app",
+    data: {
+        todos : ["buy shopping", "Clean bathroom", "MOT the car"],
+
+      },
+  });
+});
