1d7d69e97da42c9a7f707ca5dc455f146d3bec72
djghis <djghis@hotmail.com>
2020-02-24 16:30:28 +0000

    MVP met

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
-    data: {
+      data: {
         todos : ["buy shopping", "Clean bathroom", "MOT the car"],
+        newTodo: ""
 
-      },
+        },
+      methods: {
+        addNewTodo: function (){
+          this.todos.push(this.newTodo);
+          this.newTodo = "";
+        }
+      }
   });
 });
